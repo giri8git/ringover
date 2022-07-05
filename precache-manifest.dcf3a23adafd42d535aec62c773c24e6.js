7f0254e53c027448377e92e4442b78a9423b0bec
Surendra Giri <96078725+giri8git@users.noreply.github.com>
2022-07-05 11:45:22 +0530

    Updates

---
+++ precache-manifest.dcf3a23adafd42d535aec62c773c24e6.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8770ba7c54841e42acf",
+    "url": "/ringover/static/css/main.7d95b017.chunk.css"
+  },
+  {
+    "revision": "f8770ba7c54841e42acf",
+    "url": "/ringover/static/js/main.f8770ba7.chunk.js"
+  },
+  {
+    "revision": "a557779d76bdf2cae61a",
+    "url": "/ringover/static/css/1.f821556a.chunk.css"
+  },
+  {
+    "revision": "a557779d76bdf2cae61a",
+    "url": "/ringover/static/js/1.a557779d.chunk.js"
+  },
+  {
+    "revision": "9cbc2fc4c554c74970a9",
+    "url": "/ringover/static/js/runtime~main.9cbc2fc4.js"
+  },
+  {
+    "revision": "44203786ec8e2f29ead05adcf0d1ddff",
+    "url": "/ringover/static/media/shoe4.44203786.jpeg"
+  },
+  {
+    "revision": "d9f15ff27fef6fcd9604988f41791339",
+    "url": "/ringover/static/media/medium.d9f15ff2.png"
+  },
+  {
+    "revision": "2c40a217919a50e93cc2bcf2bdb50bc1",
+    "url": "/ringover/static/media/shoe3.2c40a217.jpg"
+  },
+  {
+    "revision": "b33b3dc2e7d882964e56e546f0f3bb9a",
+    "url": "/ringover/static/media/shoe1.b33b3dc2.jpg"
+  },
+  {
+    "revision": "493762fd6b57815e841e1b0d2c34c9dd",
+    "url": "/ringover/static/media/facebook.493762fd.svg"
+  },
+  {
+    "revision": "3fb273a6e81815948237f04b1769c265",
+    "url": "/ringover/static/media/insta.3fb273a6.svg"
+  },
+  {
+    "revision": "d76d114d039a0e1be6ac7709d6ff9ccf",
+    "url": "/ringover/static/media/twitter.d76d114d.svg"
+  },
+  {
+    "revision": "e85f4a994b96af8fb3d4607023095bbe",
+    "url": "/ringover/static/media/background.e85f4a99.png"
+  },
+  {
+    "revision": "99edc4d055b68cc0ea75fd423d7832a0",
+    "url": "/ringover/static/media/jrnyback.99edc4d0.jpg"
+  },
+  {
+    "revision": "e22c06dea1e563b7826a6c262db388c7",
+    "url": "/ringover/static/media/tonikroos.e22c06de.jpg"
+  },
+  {
+    "revision": "b78859038943d62598f81b3095f17c1c",
+    "url": "/ringover/static/media/Iker.b7885903.jpeg"
+  },
+  {
+    "revision": "ee71de9395969b13ed2ac561c3e5bd19",
+    "url": "/ringover/static/media/christiano.ee71de93.jpg"
+  },
+  {
+    "revision": "3e0b647a609deaa6662912b23a8f2eb7",
+    "url": "/ringover/index.html"
+  }
+];
